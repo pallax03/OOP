a7dbfb801d6e884ce1439d742852ea72c3012878
Alex Mazzoni <alexmaz03@hotmail.it>
2023-10-17 14:59:17 +0200

    Settimana 4 deleted tmp files

--- 10_16/44-45-rich-design/src/main/java/it/unibo/design/robot/api/RobotWithArms.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.design.robot.api;
-
-public interface RobotWithArms extends Robot{
-    boolean pickUp();           //picks an object, returns `true` if the action is successful
-    boolean dropDown();         //drops an object, returns `true` if the action is successful
-    int getCarriedItemsCount(); //returns the number of objects this robot is currently transporting
-}
