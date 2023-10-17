846b48c3d8953c2760591e74f3928b1cd89cfe88
Alex Mazzoni <alexmaz03@hotmail.it>
2023-10-17 15:00:52 +0200

    Settimana 4 deleted tmp files

---
+++ 10_16/41-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,29 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount{
+
+    private static final double TRANSACTION_FEE = 0.1;
+
+    public ExtendedStrictBankAccount(final int id, final double balance) {
+        super(id, balance);
+    }
+
+    public void withdraw(final int id, final double amount) {
+        if (isWithdrawAllowed(amount)) {
+            super.withdraw(id, amount);
+        }
+    }
+    
+    //TODO
+    public void chargeManagementFees(final int id) {
+        final double feeAmount = MANAGEMENT_FEE + super.getTransactionsCount() * TRANSACTION_FEE;
+        if (checkUser(id) && isWithdrawAllowed(feeAmount)) {
+            super.setBalance(super.getBalance()-feeAmount);
+            super.resetTransactions();
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount) {
+        return super.getBalance() >= amount;
+    }
+}
